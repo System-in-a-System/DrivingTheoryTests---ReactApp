047920a85ca10e7e4540243cecbd6f4122386d51
Maryna <textconstruction@protonmail.com>
2021-08-25 16:36:59 +0200

    Add questions

--- src/Data/Questions.js
+++ src/Data/Questions.js
@@ -642,14 +642,325 @@ const Questions = [
   },
 
   {
+    questionText: "Hur fort får en moped klass II tillverkad år 2015 max köra?",
+    options: [
+      ["A. Max 25 km/h", 1],
+      ["B. Max 30 km/h", 0],
+      ["C. Max 45 km/h", 0],
+    ],
+    comment: "Moped klass II är ett motorfordon på två, tre eller fyra hjul, som är konstruerat för en hastighet av högst 25 km/timmen"
+  },
+
+  {
+    questionText: "Vad innebär skyltarna till höger?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/7002.jpg",
+    options: [
+      ["A. Högerregeln gäller inte i kommande korsning.", 1],
+      ["B. Vägen är bredast till höger.", 0],
+      ["C. Vägen till vänster är enkelriktad.", 0],
+      ["D. Fordon som kommer från vänster har stopplikt.", 0],
+    ],
+    comment: "Tavlan anger en korsning där förare på en väg som anges med smalt streck har väjningsplikt mot fordon på en väg som anges med brett streck. Symbolen är anpassad efter förhållandena på platsen."
+  },
+
+  {
+    questionText: "Vad innebär imitationsinlärning?",
+    options: [
+      ["A. Att du bedömer sannolikheten för att något ska inträffa.", 0],
+      ["B. Att du söker djupgående förståelse.", 0],
+      ["C. Att du lär dig genom att studera andra.", 1],
+    ],
+    comment: "Imitationsinlärning innebär att vi ”tar till oss andra människors beteende."
+  },
+
+  {
+    questionText: "Är det generellt tillåtet att lasta ur bilen på en plats där det endast råder parkeringsförbud?",
+    options: [
+      ["A. Ja", 1],
+      ["B. Nej", 0],
+    ],
+    comment: "”Parkering är en uppställning av ett fordon med eller utan förare. Uppställningen sker av någon anledning som […] inte sker för på- eller avstigning eller på- eller avlastning av gods.” (Transportstyrelsen). Det du gör räknas alltså inte som parkering och omfattas därför inte av parkeringsförbudet."
+  },
+
+  {
+    questionText: "Du har kört på en hjort i låg hastighet och bedömer att varken djuret eller bilen har skadats. Måste du anmäla olyckan till Polisen?",
+    options: [
+      ["A. Ja", 1],
+      ["B. Nej", 0],
+    ],
+    comment: "Har ett djur av arterna björn, varg, järv, lo, älg, hjort, rådjur, utter, vildsvin, mufflonfår eller örn varit inblandat i en sammanstötning med ett motorfordon, är fordonets förare skyldig att snarast möjligt märka ut olycksplatsen och underrätta närmaste polismyndighet."
+  },
+
+  {
+    questionText: "Vad är sant angående mörkerseendet efter att du har blivit bländad av ett annat fordons helljus?",
+    options: [
+      ["A. Det påverkas inte alls.", 0],
+      ["B. Det försämras permanent.", 0],
+      ["C. Det försämras en stund men återhämtar sig snart.", 1],
+    ],
+    comment: "Efter bländning tar det någon minut i tillvänjning, innan mörkerseendet fungerar någorlunda bra igen."
+  },
+
+  {
+    questionText: "Räcker det med ett vanligt B-körkort för att med en personbil få dra en släpvagn vars totalvikt är 500 kg?",
+    options: [
+      ["A. Ja", 1],
+      ["B. Det är omöjligt att veta, eftersom mer information behövs.", 0],
+      ["C. Nej", 0],
+    ],
+    comment: "Lätt släpvagn: Släp med totalvikt på max 750 kg eller Släp där totalvikten för bil + släp är max 3,5 ton."
+  },
+
+  {
+    questionText: "Är det tillåtet att köra fortare än 50 km/h efter den blåvita skylten om du bedömer att det är lämpligt?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/6952b.jpg",
+    options: [
+      ["A. Ja", 1],
+      ["B. Nej", 0],
+    ],
+    comment: "Notera orden ”lämpligt” och ”rekommenderad”. Dessa ord signalerar inget tvång. "
+  },
+
+  {
+    questionText: "Vad betyder vägmärket?",
+    imgUrl: "https://cdn.korkortonline.se/images/vagmarken/C9-1.svg",
+    options: [
+      ["A. Förbud mot trafik med alla typer av lastbilar.", 0],
+      ["B. Förbud mot fordon lastade med farligt gods.", 1],
+      ["C. Förbud mot spårbunden trafik.", 0],
+      ["D. Förbud mot fordon lastade med flytande vätskor.", 0],
+    ],
+  },
+
+  {
+    questionText: "Vad betyder vägmärket?",
+    imgUrl: "https://cdn.korkortonline.se/images/vagmarken/D2-1.svg",
+    options: [
+      ["A. Påbjuden körriktning höger.", 0],
+      ["B. Påbjuden körbana höger.", 1],
+    ],
+    comment: ""
+  },
+
+  {
+    questionText: "Är det bra eller dåligt ur miljösynpunkt att vaxa bilen?",
+    options: [
+      ["A. Övervägande bra, eftersom det fastnar mindre smuts på lacken vilket leder till färre tvättar.", 1],
+      ["B. Övervägande dåligt, eftersom vaxet innehåller giftiga ämnen som sprids i luften.", 0],
+    ],
+  },
+
+  {
+    questionText: "Vilken formulering är bäst sett ur ett trafiksäkerhetsperspektiv?",
+    options: [
+      ["A. De andra bilarna har väjningsplikt mot mig, eftersom jag kör på en huvudled.", 1],
+      ["B. Jag har företräde, eftersom jag kör på en huvudled.", 0],
+    ],
+    comment: "Det är alltid bättre att tänka vilka skyldigheter som existerar i stället för vilka rättigheter som finns. Annars hamnar man lätt i situationer där man gör riskfyllda saker för att ”hävda sin rätt”, vilket inte alls är bra i trafiken."
+  },
+
+  {
+    questionText: "Hur kommer du bäst iväg utan hjulspinn efter att ha stannat innan en mycket hal korsning på vintern?",
+    options: [
+      ["A. Genom att växla mellan att gasa kraftigt och försiktigt flera gånger under de första meterna.", 0],
+      ["B. Genom att backa cirka en meter precis innan du ska köra ut i korsningen.", 0],
+      ["C. Genom att gasa kraftigt och jämnt.", 0],
+      ["D. Genom att gasa försiktigt och jämnt.", 1],
+    ],
+    comment: "Kraftig gas innebär större risk för hjulspinn."
+  },
+
+  {
+    questionText: "Vad ska du göra om trafikljusen i en korsning blinkar gult?",
+    imgUrl: "https://cdn.korkortonline.se/images/vagmarken/SIG5-1.svg",
+    options: [
+      ["A. Vända och ta en annan väg.", 0],
+      ["B. Stanna vid trafikljuset och vänta på att det ska bli grönt.", 0],
+      ["C. Köra försiktigt och rätta dig efter andra anvisningar eller regler.", 1],
+    ],
+    comment: "”Särskild försiktighet skall iakttas vid passage.” När trafikljusen är ur funktion ska du titta efter vägmärken eller poliser som dirigerar trafiken. Finns inget, gå då efter högerregeln."
+  },
+
+  {
+    questionText: "Vad är sant vid denna kombination av övergångsställe och cykelpassage?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/2333.jpg",
+    options: [
+      ["A. Det är tillåtet att köra om tvåhjuliga fordon i samband med övergångsstället och cykelpassagen.", 0],
+      ["B. Eftersom cykelpassagen är obevakad har jag väjningsplikt mot alla som korsar vägen på cykelpassagen.", 0],
+      ["C. Jag kan bli skyldig att sänka hastigheten om en cyklist börjar korsa vägen på cykelpassagen.", 1],
+      ["D. Det är tillåtet att parkera inom ett avstånd av 10 meter före övergångsstället och cykelpassagen.", 0],
+    ],
+    comment: "Fordon eller spårvagnar får inte köras om strax före eller på ett obevakat övergångsställe, en obevakad cykelpassage eller en cykelöverfart. Du har endast väjningsplikt mot gående, inte cyklister."
+  },
+
+  {
+    questionText: "Vilket alternativ är inte en vanlig typ av broms på personbilar?",
+    options: [
+      ["A. Skivbroms", 0],
+      ["B. Fjäderbroms", 1],
+      ["C. Trumbroms", 0],
+    ],
+    comment: "Skivbromsar och trumbromsar är de dominerande bromstyperna för personbilar. Trumbromsar på bakhjulen och skivbromsar på framhjulen är en vanlig kombination. ”Fjäderbroms” är det svår att överhuvudtaget hitta information om, vilket gör att den inte är en vanlig bromstyp, som var det frågan gällde."
+  },
+
+  {
+    questionText: "Vilken släpvagnstyp är tillåten att dra om du endast har körkort med behörighet B?",
+    options: [
+      ["A. Endast obromsad släpvagn.", 0],
+      ["B. Endast lätt släpvagn.", 1],
+      ["C. Endast bromsad släpvagn.", 0],
+    ],
+    comment: "Det räcker med behörigheten B för att köra personbil eller lätt lastbil med ett lätt släp. I andra fall krävs behörighet för utökad B eller BE. Lätta släpvagnar finns både som bromsade och obromsade."
+  },
+
+  {
+    questionText: "Fotot är taget från bilen du kör. Vem ska stanna och lämna företräde enligt reglerna?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/2861.jpg",
+    options: [
+      ["A. Du ska lämna företräde åt den röda bilen eftersom det finns mest plats till höger.", 0],
+      ["B. Den röda bilen ska lämna företräde åt dig eftersom alla hinder finns på den bilens sida.", 1],
+    ],
+    comment: "Om det finns något hinder för möte skall den förare stanna som har hindret på sin sida, om det är nödvändigt för att den mötande skall kunna passera."
+  },
+
+  {
+    questionText: "Det är mörkt ute och du kör med halvljus. På vilket avstånd upptäcker du en fotgängare med mörka kläder utan reflex?",
+    options: [
+      ["A. Cirka 25 meter.", 1],
+      ["B. Cirka 50 meter.", 0],
+      ["C. Cirka 75 meter.", 0],
+      ["D. Cirka 100 meter.", 0],
+    ],
+  },
+
+  {
+    questionText: "Du kör tunga varor åt en vän. Innan vännens hus sitter detta märke. Vad gäller?",
+    imgUrl: "https://cdn.korkortonline.se/images/vagmarken/D5-1.svg",
+    options: [
+      ["A. Du får alltid köra in där märket sitter, men endast i gångfart.", 0],
+      ["B. Du får köra in där märket sitter eftersom du ska lasta ur varor.", 0],
+      ["C. Du får inte köra in där märket sitter.", 1],
+    ],
+    comment: "Märket anger bana avsedd endast för gående."
+  },
+
+  {
+    questionText: "Har du väjningsplikt mot cyklister som korsar vägen på övergångsstället?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/2271.jpg",
+    options: [
+      ["A. Ja", 0],
+      ["B. Nej", 1],
+    ],
+    comment: "En cyklande som ändå korsar en väg på ett övergångsställe har alltid väjningsplikt både mot gående och mot fordon på vägen."
+  },
+
+  {
+    questionText: "Vad betyder vägmärket?",
+    imgUrl: "https://cdn.korkortonline.se/images/vagmarken/C4-1.svg",
+    options: [
+      ["A. Förbud mot trafik med alla motordrivna fordon.", 0],
+      ["B. Förbud mot trafik med motordrivna fordon med fler än två hjul.", 1],
+      ["C. Förbud mot all fordonstrafik.", 0],
+    ],
+  },
+
+  {
+    questionText: "Vad innebär denna märkeskombination?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/b3-t9.svg",
+    options: [
+      ["A. Olycksdrabbat övergångsställe.", 0],
+      ["B. Övergångsställe där personer med nedsatt hörsel är vanligt förekommande.", 0],
+      ["C. Övergångsställe där personer med nedsatt syn är vanligt förekommande.", 1],
+      ["D. Bevakat övergångsställe.", 0],
+    ],
+  },
+
+  {
+    questionText: "Vad är sant 15 meter efter denna korsning?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/4613.jpg",
+    options: [
+      ["A. Jag får parkera på körbanan.", 0],
+      ["B. Jag får stanna på körbanan för att släppa av en passagerare.", 1],
+      ["C. Jag får inte stanna på körbanan, eftersom det är en huvudled.", 0],
+      ["D. Jag får inte stanna på körbanan, eftersom det är 15 meter efter en korsning.", 0],
+    ],
+    comment: "Det är förbjudet att parkera på en huvudled, men det finns inget förbud mot att stanna på en huvudled. När det gäller en korsning, det är förbjudet att stanna inom 10 meter från en korsning (15 meter efter är tillåtet)."
+  },
+
+  {
+    questionText: "Är det sant att du alltid måste använda vinterdäck om det råder vinterväglag?",
+    options: [
+      ["A. Ja, alltid", 0],
+      ["B. Nej, inte exempelvis 1 april", 1],
+    ],
+    comment: "När du kör personbil […] är det krav på vinterdäck […] 1 december–31 mars, om det är vinterväglag.” (Transportstyrelsen). Det betyder att det inte är något lagkrav på att ha vinterdäck om det är snö och is den 1 april. Formuleringen ”om det är vinterväglag” gäller endast ihop med 1 december–31 mars. Annars skulle det stå till exempel: ”krav på vinterdäck 1 december–31 mars eller om det är vinterväglag."
+  },
+
+  {
+    questionText: "Vilket märke anger det maximalt tillåtna axeltrycket?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/c20-c21-c23-c24.svg",
+    options: [
+      ["A. ", 0],
+      ["B. ", 0],
+      ["C. ", 1],
+      ["D. ", 0],
+    ],
+  },
+
+  {
+    questionText: "Vilket är det maximalt tillåtna axeltrycket på den kommande vägen till höger?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/6669.jpg",
+    options: [
+      ["A. 4 ton", 1],
+      ["B. 8 ton", 0],
+      ["C. 12 ton", 0],
+      ["D. 24 ton", 0],
+    ],
+  },
+
+  {
+    questionText: "Får en buss i linjetrafik svänga vänster?",
+    imgUrl: "https://cdn.korkortonline.se/images/teoritest/2626.jpg",
+    options: [
+      ["A. Ja", 1],
+      ["B. Nej", 0],
+    ],
+    comment: "Märkena ska tolkas som: ”Buss i linjetrafik får välja väg helt fritt. Alla andra måste dock svänga vänster.”"
+  },
+
+  {
+    questionText: "Vad är sant angående vägmärket?",
+    imgUrl: "https://cdn.korkortonline.se/images/vagmarken/D8-1.svg",
+    options: [
+      ["A. Gångtrafik är tillåten.", 1],
+      ["B. Märket varnar för hästar.", 0],
+      ["C. Endast hästar får passera märket.", 0],
+      ["D. Endast hästar med ryttare får passera märket.", 0],
+    ],
+  },
+
+  {
+    questionText: "Kan olika kommuner ha olika regler för exempelvis parkering?",
+    options: [
+      ["A. Nej, trafikreglerna är exakt samma i hela landet.", 0],
+      ["B. Ja, olika lokala bestämmelser förekommer.", 1],
+    ],
+    comment: "Särskilda trafikregler får […] meddelas genom lokala trafikföreskrifter […] De särskilda trafikreglerna får gälla […] Förbud mot att stanna eller parkera fordon eller tillåtelse att stanna eller parkera fordon med avvikelse från bestämmelserna."
+  },
+
+  {
     questionText: "",
+    imgUrl: "",
     options: [
       ["A. ", 0],
       ["B. ", 0],
       ["C. ", 0],
       ["D. ", 0],
     ],
+    comment: ""
   },
+
+
 ];
 
 export default Questions;
