e3807c96f988d086062d6d4adc9123ca66e05f59
Maryna <textconstruction@protonmail.com>
2021-08-25 14:31:23 +0200

    Add scroll up. verdict calculation and comment sections on submit

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -5,11 +5,12 @@ import "./Quiz.css";
 
 const questionPortion = questions
     .sort(() => Math.random() - 0.5)
-    .slice(0, 10);
+    .slice(0, 25);
     
 
 const Quiz = ({ name, score, setScore }) => {
   const [disable, setDisable] = useState(false);
+  const [pass, setPass] = useState(0);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -28,31 +29,54 @@ const Quiz = ({ name, score, setScore }) => {
           setScore((score += 1));
           radiosValue.nextSibling.style.color = "green";
           radiosValue.nextSibling.style.fontWeight = "bold";
+
         } else if (!radiosValue.checked && radiosValue.value === "1") {
           radiosValue.nextSibling.style.color = "green";
           radiosValue.nextSibling.style.fontWeight = "bold";
           radiosValue.nextSibling.style.fontStyle = "italic";
+
         } else if (radiosValue.checked && radiosValue.value === "0") {
           radiosValue.nextSibling.style.color = "red";
           radiosValue.nextSibling.style.fontWeight = "bold";
-          const commentSection = document.getElementById("commentSection");
-          commentSection.style.display = "block";
+          
         } else {
           radiosValue.nextSibling.style.color = "";
         }
       }
     }
     setDisable(true);
+
+    let commentSections = document.getElementsByClassName("commentSection");
+    for (let i = 0; i < commentSections.length; i++) {
+      const comment = commentSections[i];
+      comment.style.display = "block";
+    }
+
+    const testPassed = (score/questionPortion.length) * 100 >= 85;
+    if(testPassed) {
+      setPass(1);
+    } else {
+      setPass(2);
+    }
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   let currentQuestion = 1;
 
   return (
     <div className="wrapper">
+      
       <form className="quiz" onSubmit={handleSubmit}>
+        
         <div className="quizInformation">
           <span>Student: {name}</span>
           <span>Score : {score}</span>
+
+          { // eslint-disable-next-line no-sequences
+            pass === 1 && <div style={{ fontWeight: "bolder"}, { color: "green"}, { marginTop: "10px"}}>Verdict: Approved ({(score/questionPortion.length) * 100} %)</div> }
+          { // eslint-disable-next-line no-sequences
+            pass === 2 && <div style={{ fontWeight: "bolder"}, { color: "red"}, { marginTop: "10px"}}>Verdict: Unapproved ({((score/questionPortion.length) * 100)} %)</div> }  
         </div>
 
         {questionPortion.map((question) => (
@@ -60,12 +84,12 @@ const Quiz = ({ name, score, setScore }) => {
             <div className="questionText">{question.questionText}</div>
             {question.imgUrl && (
                 // eslint-disable-next-line no-sequences
-                <div id="imageHolder" style={{ height: "300px" }, { margin: "10px" }}>
+                <div id="imageHolder" style={{ height: "300px" }, { margin: "10px" }, {objectFit: "contain"}, { overflow: "hidden"}}>
                   <img 
                     alt="illustration"
                     src = {question.imgUrl}
                     // eslint-disable-next-line no-sequences
-                    style = {{ height: "295px" }, { margin: "0"}, { objectFit: "contain"}}
+                    style = {{ height: "295px" }, { margin: "0"}, { objectFit: "contain"}, { overflow: "hidden"}}
                   />
                 </div>
               )}
@@ -117,7 +141,7 @@ const Quiz = ({ name, score, setScore }) => {
               }
 
               {question.comment && (
-                <div id="commentSection" style={{ display: "none" }}>
+                <div className="commentSection" style={{ display: "none" }}>
                   {question.comment}    
                 </div>
               )}
