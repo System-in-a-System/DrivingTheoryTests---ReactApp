1ea423f11039b8a25b757241023d87d6c231e8a1
Maryna <textconstruction@protonmail.com>
2021-08-25 16:36:35 +0200

    Update title

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 
 const Header = () => {
     return <div className="header">
-        <Link to="/" className="title">Head To The Aim</Link>
+        <Link to="/" className="title">DRIVE AHEAD</Link>
     </div>;
 };
 
